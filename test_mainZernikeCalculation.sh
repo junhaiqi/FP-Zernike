cc6bd60ad842f035cbbe4a3a0e34c436d9ebf9ad
qjh <91476077+junhaiqi@users.noreply.github.com>
2022-11-09 20:59:08 +0800

    Update test_mainZernikeCalculation.sh

--- test_mainZernikeCalculation.sh
+++ test_mainZernikeCalculation.sh
@@ -1,4 +1,4 @@
-python mainZernikeCalculation.py -i example/4m2qA.pdb -file_source atom -mode atom -o test_4m2qA_ATOM.pkl
-python mainZernikeCalculation.py -i example/4m2qA.pdb -file_source pymol -mode mesh -o test_4m2qA_PM.pkl
-python mainZernikeCalculation.py -i example/4m2qA.pdb -file_source pymol -mode surface -o test_4m2qA_PS.pkl
-python mainZernikeCalculation.py -i example/4m2qA.pdb -file_source gmconvert -mode GM -o test_4m2qA_GM.pkl
+python mainZernikeCalculation.py -i example/4mu3A.pdb -file_source atom -mode atom -o test_4mu3A_ATOM.pkl
+python mainZernikeCalculation.py -i example/4mu3A.pdb -file_source pymol -mode mesh -o test_4mu3A_PM.pkl
+python mainZernikeCalculation.py -i example/4mu3A.pdb -file_source pymol -mode surface -o test_4mu3A_PS.pkl
+python mainZernikeCalculation.py -i example/4mu3A.pdb -file_source gmconvert -mode GM -o test_4mu3A_GM.pkl
